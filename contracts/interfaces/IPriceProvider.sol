8b70abdc70f11d5c0fef6afa8afb4fad07dfdd12
vyomshm <vyomshm@gmail.com>
2021-06-23 23:10:53 +0530

    fix oracle bugs

--- contracts/interfaces/IPriceProvider.sol
+++ contracts/interfaces/IPriceProvider.sol
@@ -1,6 +1,6 @@
-// SPDX-License-Identifier: agpl-3.0
+// SPDX-License-Identifier: GPL-3.0
 
-pragma solidity 0.8.0;
+pragma solidity ^0.8.0;
 
 interface IPriceProvider {
     function setAssetFeeds(address[] memory _assets, address[] memory _feeds) external;
